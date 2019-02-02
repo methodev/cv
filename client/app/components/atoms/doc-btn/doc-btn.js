776fc45f99615ffd4f5f350141b19bda2d5df3be
Martin Metodiev <metodiev.martin@gmail.com>
2019-02-02 12:17:34 +0200

    Add ReactTooltip in DocBtn component

--- client/app/components/atoms/doc-btn/doc-btn.js
+++ client/app/components/atoms/doc-btn/doc-btn.js
@@ -7,6 +7,7 @@
 // Libraries
 import React from 'react';
 import classNames from 'classnames';
+import ReactTooltip from 'react-tooltip';
 
 // Styles
 import styles from './doc-btn.scss';
@@ -24,15 +25,22 @@ const DocBtn = ({
   const Icon = () => icon;
 
   return (
-    <a
-      className={classNames(styles.root, styles[id])}
-      onClick={handler}
-      title={tooltip}
-      href={link}
-      target={link ? '_blank' : null}
-    >
-      <Icon />
-    </a>
+    <>
+      <a
+        className={classNames(styles.root, styles[id])}
+        onClick={handler}
+        data-tip={tooltip}
+        data-for={'pdf'}
+        href={link}
+        target={link ? '_blank' : null}
+      >
+        <Icon />
+      </a>
+      <ReactTooltip
+        id={'pdf'}
+        place={'left'}
+      />
+    </>
   );
 };
 
