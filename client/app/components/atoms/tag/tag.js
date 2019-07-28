44bb81464f1904eb73875a5e30fef1e366ecf0d4
Martin Metodiev <metodiev.martin@gmail.com>
2019-07-28 15:26:12 +0300

    Add links support to Tags

--- client/app/components/atoms/tag/tag.js
+++ client/app/components/atoms/tag/tag.js
@@ -17,29 +17,40 @@ import styles from './tag.scss';
 
 //--------------------------| Component
 
-const Tag = ({ data }) => (
-  <>
-  <span className={styles.root} data-tip data-for={data.sys.id}>
-    {data.fields.name}
-  </span>
-    {
-      (data.fields.description || data.fields.experience) && (
-        <Tooltip
-          id={data.sys.id}
-          place={'top'}
+const Tag = ({ data }) => {
+  const TagWrapper = data.fields.website ? 'a' : 'span';
+  return (
+    <>
+      <span className={styles.root}>
+        <TagWrapper
+          href={data.fields.website}
+          target={data.fields.website ? '_blank' : null}
+          data-tip
+          data-for={data.sys.id}
         >
-          <p>{data.fields.description}</p>
-          { data.fields.experience && (
-            <div className={'bar'}>
-              <ProgressBar level={data.fields.experience} label={data.fields.label} />
-            </div>
-          )
-          }
-        </Tooltip>
-      )
-    }
-  </>
-);
+          {data.fields.name}
+        </TagWrapper>
+      </span>
+      {
+        (data.fields.description || data.fields.experience) && (
+          <Tooltip
+            id={data.sys.id}
+            place={'top'}
+          >
+            <p>{data.fields.description}</p>
+            {
+              data.fields.experience && (
+                <div className={'bar'}>
+                  <ProgressBar level={data.fields.experience} label={data.fields.label} />
+                </div>
+              )
+            }
+          </Tooltip>
+        )
+      }
+    </>
+  );
+};
 
 
 //--------------------------| Export
