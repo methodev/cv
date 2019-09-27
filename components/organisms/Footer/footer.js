bf48c57c78ebb6116a7f87abd0a3318f865d693c
Martin Metodiev <metodiev.martin@gmail.com>
2019-09-27 14:30:45 +0300

    Restore PDF button in the Footer

--- components/organisms/Footer/footer.js
+++ components/organisms/Footer/footer.js
@@ -1,8 +1,18 @@
 // ===================================================|
 // Organisms: FOOTER
 
+import { mapState } from 'vuex';
+
+// Components
 import Copyright from '@/components/atoms/Copyright';
+import DocBtn from '@/components/atoms/DocBtn';
 
 export default {
-  components: { Copyright }
+  components: { Copyright, DocBtn },
+
+  computed: {
+    ...mapState({
+      pdf: ({ contentful }) => contentful.data.pdfVersion.fields.file.url
+    })
+  }
 };
