909de3f4d2d7ffa5a8d9e1b4214d787f56ac6bce
Martin Metodiev <metodiev.martin@gmail.com>
2019-09-29 21:11:52 +0300

    Set the Nuxt mode back to "universal"

--- store/contentful.js
+++ store/contentful.js
@@ -3,8 +3,7 @@ import { contentfulLayout } from '~/site.json';
 
 export const state = () => ({
   locales: {},
-  data: {},
-  storage: {}
+  data: {}
 });
 
 export const mutations = {
@@ -14,10 +13,6 @@ export const mutations = {
 
   SET_DATA(state, payload) {
     state.data = payload;
-  },
-
-  SET_STORAGE(state, { locale, data }) {
-    state.storage[locale] = data;
   }
 };
 
@@ -36,20 +31,11 @@ export const actions = {
   },
 
   async fetchContent({ state, commit }, { locale }) {
-    if (state.storage && state.storage[locale]) {
-      return commit('SET_DATA', state.storage[locale]);
-    }
-
     try {
       const response = await getContent({ locale });
       const data = response.items
         .filter((item) => item.sys.contentType.sys.id === 'cv')
         .filter((cv) => cv.fields.id === contentfulLayout)[0].fields;
-
-      commit('SET_STORAGE', {
-        locale,
-        data
-      });
 
       return commit('SET_DATA', data);
     } catch (e) {
