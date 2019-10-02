a08d80107af84d62248a059dfa9d6f31f3c69047
Martin Metodiev <metodiev.martin@gmail.com>
2019-10-02 17:48:37 +0300

    Update "mm-atomic-pack" to v1.10.0

--- components/atoms/Copyright/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="./copyright.html"></template>
-<script src="./copyright.js"></script>
-<style src="./copyright.scss" module lang="scss"></style>
--- components/organisms/Footer/footer.js
+++ components/organisms/Footer/footer.js
@@ -2,9 +2,10 @@
 // Organisms: FOOTER
 
 import { mapState } from 'vuex';
+import { homepage } from '@/package';
 
 // Components
-import Copyright from '@/components/atoms/Copyright';
+import Copyright from '@/node_modules/mm-atomic-pack/components/atoms/Copyright';
 import DocBtn from '@/components/atoms/DocBtn';
 
 export default {
@@ -13,6 +14,9 @@ export default {
   computed: {
     ...mapState({
       pdf: ({ contentful }) => contentful.data.pdfVersion.fields.file.url
-    })
+    }),
+    domain() {
+      return homepage.replace('http://', '');
+    }
   }
 };
