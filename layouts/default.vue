3d536a84f0557a51adb35d4057b418a4794fb7ce
Martin Metodiev <metodiev.martin@gmail.com>
2019-10-12 11:25:22 +0300

    Optimize layout to use "cv-assets"

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,35 @@
-<template src="./default.html"></template>
-<script src="./default.js"></script>
-<style src="./default.scss" module lang="scss"></style>
+<template src="@/node_modules/cv-assets/layouts/default.html"></template>
+
+<script>
+import { mapState } from 'vuex';
+import { filterLocales } from '@/node_modules/cv-assets/services/FilterService';
+
+// Components
+import LangSwitcher from '@/node_modules/cv-assets/components/atoms/LangSwitcher';
+import CircleButton from '@/node_modules/cv-assets/components/atoms/CircleButton';
+import Header from '@/node_modules/cv-assets/components/organisms/Header';
+import Footer from '@/node_modules/mm-atomic-pack/components/organisms/Footer';
+
+// Graphics
+import FileSVG from '@/node_modules/cv-assets/assets/graphics/pdf.svg';
+
+export default {
+  components: { LangSwitcher, CircleButton, Header, Footer, FileSVG },
+
+  computed: {
+    ...mapState({
+      locales: ({ contentful }) => contentful.locales,
+      pdf: ({ contentful }) => contentful.data.pdfVersion.fields.file.url
+    }),
+    languages() {
+      return filterLocales(this.locales, this.$i18n.locale);
+    }
+  }
+};
+</script>
+
+<style
+  src="@/node_modules/cv-assets/layouts/default.scss"
+  module
+  lang="scss"
+></style>
